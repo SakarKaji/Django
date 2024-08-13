31b8a5bfce62f078706f43f188a9e936da904094
sakarrr <sakarkd@gmail.com>
2024-08-13 17:45:49 +0545

    get/read process complete

--- crudapi/api/views.py
+++ crudapi/api/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+import io
+from rest_framework.parsers import JSONParser
+from .models import Student
+from .serializers import StudentSerializers
+from rest_framework.renderers import JSONRenderer 
 
 # Create your views here.
+def student_api(request):
+    if request.method == 'GET':
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        pythonData = JSONParser().parse(stream)
+        id = pythonData.get('id', None) #all client data ie id or none id , as a python dictionary
+        if id is not None:
+            stu = Student.objects.get(id=id)
+            serializer = StudentSerializers(stu)
+            json_data = JSONRenderer().render(serializer.data)
+            return HttpResponse(json_data, content_type='application/json')
+        
+        # else / id is none / id have no data
+        stu = Student.objects.all()
+        serializer - StudentSerializers(stu, many=True)
+        json_data = JSONRenderer().render(serializer.data)
+        return HttpResponse(json_data, content_type='application/json')
+
+        
+
+
+
+
